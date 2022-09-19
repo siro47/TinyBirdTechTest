7b5eff26562961a78a6a84f31f42642b5620650f
Siro Ramírez <darksaiyan@MacBook-Air-de-Siro.local>
2022-09-19 13:41:27 +0200

    Added support for multithreading. Added batch processing. Added temporal files management

--- app.py
+++ app.py
@@ -4,24 +4,31 @@ import json
 import sys
 import tornado.ioloop
 import tornado.web
+from tornado.process import Subprocess
 
 from datetime import date
+import uuid
 
 
 @tornado.web.stream_request_body
 class DataReceiverHandler(tornado.web.RequestHandler):
     def initialize(self):
         self.request_body = b''
+        self.uniqueid = self.request.headers.get("request-id") or str(uuid.uuid4())
 
     def data_received(self, chunk):
         self.request_body += chunk
 
     def post(self):
+        BATCH_SIZE = 1000
+        
+        BASE_FILE_NAME = f"csv/nyc_taxi-{date.today()}"
+        DAILY_FILE_NAME = f"{BASE_FILE_NAME}.csv"
+        TEMPORAL_FILE_NAME = f"{BASE_FILE_NAME}-{self.uniqueid}.csv"
+
         records_valid = 0
         records_invalid = 0
-        request_id = self.request.headers.get("request-id") or str(uuid.uuid4())
-        print(request_id)
-        """
+        
         with open(f"csv/nyc_taxi-{date.today()}.csv", 'a') as fw:
             fieldnames = [
                 'vendorid',
@@ -31,18 +38,42 @@ class DataReceiverHandler(tornado.web.RequestHandler):
             ]
             writer = csv.DictWriter(fw, fieldnames, extrasaction='ignore')
             fr = io.BytesIO(self.request_body)
-            lines = fr.readlines()
-            # writer.writerows(lines)
             
+            rows = []
+            counter = 0
+
+            def reset_batch():
+                rows.clear()
+                counter = 0
+
             for record in fr.readlines():
                 try:
-                    row = json.loads(record)
-                    writer.writerow(row)
-                    records_valid += 1
+                    rows.append(json.loads(record))
+                    counter +=1
+                    if (counter >= BATCH_SIZE):
+                        writer.writerows(rows)
+                        records_valid += len(rows)
+                        reset_batch()
                 except Exception:
-                    records_invalid += 1
+                    records_invalid +=1
             
+            # Store last batch
+            writer.writerows(rows)
+            records_valid += len(rows)
 
+        mergecmd = f"cat {TEMPORAL_FILE_NAME} > {DAILY_FILE_NAME}"
+        deletecmd = f"rm {TEMPORAL_FILE_NAME}"
+
+        STREAM = Subprocess.STREAM
+
+        process = Subprocess(
+            mergecmd, stdin=STREAM, stdout=STREAM, stderr=STREAM, shell=True
+        )
+        
+        process.set_exit_callback(
+            lambda retcode: Subprocess(deletecmd, stdin=STREAM, stdout=STREAM, stderr=STREAM, shell=True)
+        )     
+        
         result = {
             'result': {
                 'status': 'ok',
@@ -57,8 +88,6 @@ class DataReceiverHandler(tornado.web.RequestHandler):
             }
         }
         self.write(result)
-        """
-
 
 def run():
     handlers = [
@@ -71,7 +100,10 @@ def run():
     port = 8888
     address = '0.0.0.0'
     application = tornado.web.Application(handlers, **settings)
-    application.listen(port, address)
+    server = tornado.httpserver.HTTPServer(application, max_buffer_size=1024*1024*201)
+    server.bind(port)
+    server.start(0)
+
     print(f"server listening at {address}:{port} debug={debug}")
     tornado.ioloop.IOLoop.current().start()
 
